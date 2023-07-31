95011b33c51521db2110341c6e2943856c5d949c
sakkkkkk <anoussak.hanvichid4@gmail.com>
2023-07-31 09:24:05 +0200

    one test added and bonus for atout 14 corrected in TotalScoreServiceImplTest

--- src/test/java/com/soprasteria/serviceImplTest/TotalScoreServiceImplTest.java
+++ src/test/java/com/soprasteria/serviceImplTest/TotalScoreServiceImplTest.java
@@ -15,17 +15,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class TotalScoreServiceImplTest {
 
-    private final Integer POINTS_PER_FOLD_IF_BET_EQUALS_0 = 10;
-    private final Integer POINTS_PER_FOLD_IF_BET_SUP_TO_0_FAILED = -10;
+    private final Integer POINTS_PER_FOLD_IF_BET_EQUALS_0_SUCCESS = 10;
+    private final Integer POINTS_PER_FOLD_IF_BET_SUP_OR_EQUALS_TO_0_FAILED = -10;
     private final Integer POINTS_PER_FOLD_IF_BET_SUP_TO_0_SUCCESS = 20;
     private final Integer POINTS_PER_COLOR_VALUE_14_COLLECTED = 10;
     private final Integer POINTS_PER_PIRATE_COLLECTED = 30;
-    private final Integer POINTS_FOR_ATOUT_VALUE_14_COLLECTED = 30;
+    private final Integer POINTS_FOR_ATOUT_VALUE_14_COLLECTED = 20;
 
     private final TotalScoreServiceImpl totalScoreManagement = new TotalScoreServiceImpl();
 
     @Test
-    void test_total_score_30_if_bet_0_fold_success_AND_bonus_0() {
+    void test_total_score_30_if_bet_0_fold_success() {
         // Given
         List<Fold> folds = new ArrayList<>();
         Player player = new Player(1, "Anoussak", 0, folds);
@@ -35,7 +35,28 @@ public class TotalScoreServiceImplTest {
         int totalScore = totalScoreManagement.totalScore(player, setNumber);
 
         // Then
-        assertThat(totalScore).isEqualTo(setNumber * POINTS_PER_FOLD_IF_BET_EQUALS_0);
+        assertThat(totalScore).isEqualTo(setNumber * POINTS_PER_FOLD_IF_BET_EQUALS_0_SUCCESS);
+    }
+
+    @Test
+    void test_total_score_minus_30_if_bet_0_fold_failed() {
+        // Given
+        List<Card> cards = new ArrayList<>();
+        cards.add(0, new Card(NameCardEnum.Yellow.name(), 5));
+        cards.add(1, new Card(NameCardEnum.Purple.name(), 14));
+        cards.add(2, new Card(NameCardEnum.Green.name(), 14));
+
+        List<Fold> folds = new ArrayList<>();
+        folds.add(0, new Fold(cards));
+
+        Player player = new Player(1, "Anoussak", 0, folds);
+        int setNumber = 3;
+
+        // When
+        int totalScore = totalScoreManagement.totalScore(player, setNumber);
+
+        // Then
+        assertThat(totalScore).isEqualTo(setNumber * POINTS_PER_FOLD_IF_BET_SUP_OR_EQUALS_TO_0_FAILED);
     }
     
     @Test
@@ -77,7 +98,7 @@ public class TotalScoreServiceImplTest {
         int totalScore = totalScoreManagement.totalScore(player, setNumber);
 
         // Then
-        assertThat(totalScore).isEqualTo(abs(player.getBet() - setNumber) * POINTS_PER_FOLD_IF_BET_SUP_TO_0_FAILED);
+        assertThat(totalScore).isEqualTo(abs(player.getBet() - setNumber) * POINTS_PER_FOLD_IF_BET_SUP_OR_EQUALS_TO_0_FAILED);
     }
 
     @Test
